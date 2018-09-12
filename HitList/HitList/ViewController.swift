c3af7c4862b245d9822686a1b123de9d1fb64c20
Deo Kalule <Deograstius@gmail.com>
2018-09-12 04:40:46 -0400

    Finished with add name functionality

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -8,18 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    // Added tableView dataSource from storyboard
+    @IBOutlet weak var tableView: UITableView!
+    
+    // The names in the array
+    var names: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        names = [String]()
+        
+    }// End viewDidLoad() Method
+    
+    // Pops up an alert and allows the user to enter a name
+    @IBAction func addName(_ button: UIBarButtonItem){
+        
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+            
+            // Get the alert textField and set the nameToSave equal to what is in that textField
+            guard let textField = alert.textFields?.first,
+                  let nameToSave = textField.text else {
+                    
+                return
+            }
+            
+            //Save the name and reload the data
+            self.names.append(nameToSave)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField()
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default))
+        alert.addAction(saveAction)
+        
+        present(alert, animated: true)
+        
+    }// End addName(_:) Method
+    
+    
+    // Returns 1 for the single section
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }// End numberOfSections(in: ) Method
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Dequeue a cell and add a name
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+        
+        
+    }// End tableView(_: cellForRowAt: ) Method
+    
+    
+    // Returns the number of names as the row count
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }// End tableView(_: numberOfRowsInSection:) Method
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
+}// End ViewController class
 
